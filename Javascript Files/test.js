7fe3ebe994b9e70527d093b91336b7ea8bd29dff
MartinDowell <martin_te_dowell@dsl.pipex.com>
2018-01-31 13:23:43 +0000

    With average function

--- Javascript Files/test.js
+++ Javascript Files/test.js
@@ -13,6 +13,11 @@ function birth(p)  {
   return p.age();
 }
 
+function average(arr)  {
+  function plus(a,b)  { return a + b }
+  return arr.reduce(plus) / arr.length
+}
+
 var thomas = new Person('Thomas', 1810, 1873);
 var william = new Person('William', 1753, 1811);
 var joseph = new Person('Joseph', 1725, 1761);
@@ -23,3 +28,4 @@ arr.push(william);
 arr.push(joseph);
 var splat = arr.map(birth);
 console.log(splat);
+console.log(average(splat));
