86a852ae1668ac28fbcf4f0e2c26677e4bf85402
Martin <martin_te_dowell@dsl.pipex.com>
2018-02-01 12:18:53 +0000

    More entries added

--- Javascript Files/test.js
+++ Javascript Files/test.js
@@ -9,8 +9,12 @@ Person.prototype.age = function()  {
   return (this.died - this.born);
 };
 
-function birth(p)  {
+function ages(p)  {
   return p.age();
+}
+
+function birth(p)  {
+  return p.born;
 }
 
 function average(arr)  {
@@ -21,12 +25,22 @@ function average(arr)  {
 var thomas = new Person('Thomas', 1810, 1873);
 var william = new Person('William', 1753, 1811);
 var joseph = new Person('Joseph', 1725, 1761);
+//var james = new Person('James', 1888, 1951);
+//var richard = new Person('Richard', 1866, 1922);
+
 thomas.birthplace = 'Mancetter';
 
 arr.push(thomas);
 arr.push(william);
 arr.push(joseph);
-var splat = arr.map(birth);
-//splat is temporary variable
+//arr.push(james);
+//arr.push(richard);
+
+var splat = arr.map(ages);
+var splat2 = arr.map(birth);
+//With more entries added
+
 console.log('The ages are: ' + splat);
 console.log('The average age is: ' + average(splat));
+console.log('The years of brth are: ' + splat2);
+console.log('The average birth year is: ' + average(splat2));
