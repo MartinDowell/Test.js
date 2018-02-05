842822d266f7aea786146662f0f9ff3ae174ec60
Martin <martin_te_dowell@dsl.pipex.com>
2018-02-05 11:44:00 +0000

    Version 1

--- Javascript Files/test.js
+++ Javascript Files/test.js
@@ -1,33 +1,13 @@
-function Vector (x, y) {
-this.x = x;
-this.y = y;
-}
-Vector.prototype.plus = function (other) {
-return new Vector (this.x + other.x, this.y + other.y);
-};
+const arr = [];
 
-function Grid (width,height) {
-this.space = new Array (width * height) ;
-this.width = width ;
-this.height = height;
-};
+arr.push('apple');
+arr.push('pear');
+arr.push('plum');
+arr.push('cherry');
+console.log(arr[1]);
+var o = {};
 
-Grid.prototype.isInside = function (vector) {
-return vector.x >= 0 && vector.x < this.width &&
-vector.y >= 0 && vector.y < this.height;
+for (i = 0; i <= arr.length; i++)  {
+  o.fruit = arr[i];
 };
-
-Grid.prototype.get = function (vector) {
-return this.space [vector.x + this.width * vector.y];
-};
-
-Grid.prototype.set = function ( vector , value ) {
-this.space [vector.x + this.width * vector.y] = value ;
-};
-
-var grid = new Grid (5, 5);
-console.log (grid.get(new Vector (1, 1)));
-// ! u n d e f i n e d
-grid.set ( new Vector (1, 1), " X ");
-console.log (grid.get ( new Vector (1, 1)));
-// ! X
+console.log(o);
