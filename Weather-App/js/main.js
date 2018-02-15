0f97c0b209991f29c36b6d3072163fc377c86de8
Martin <martin_te_dowell@dsl.pipex.com>
2018-02-15 22:58:42 +0000

    Latest

--- Weather-App/js/main.js
+++ Weather-App/js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-   //$("button-current").click(function()  {
-
     var xhttp = new XMLHttpRequest();
     var url = "http://api.apixu.com/v1/current.json?key=ee63afa5ce654c989c6120300172106&q=cv37";
 
@@ -12,20 +10,24 @@ $(document).ready(function() {
     };
     xhttp.open("GET", url, false);
     xhttp.send();
-  //})
-
-  // function getWeather(jsonRet)  {
-  //   var jsonDoc = JSON.parse(jsonRet.responseText);
-  //   document.getElementById("current").innerHTML = jsonDoc.current.condition.text;
-  //   document.getElementById("temp").innerHTML = jsonDoc.current.temp_c;
-  //   document.getElementById("windspeed").innerHTML = jsonDoc.current.wind_mph;
-
-}
 
 
+  function getWeather(jsonRet)  {
+    var jsonDoc = JSON.parse(jsonRet.responseText);
+    document.getElementById('current').innerHTML = jsonDoc.current.temp_c +'&#8451';
+    document.getElementById('location').innerHTML = jsonDoc.location.name;
 
+    var dateTime = jsonDoc.location.localtime;
+    var date = dateTime.split(' ');
+    var time = date[1];
+    var dateSep = date[0].split('-');
+    var year = dateSep[0];
+    var month = dateSep[1];
+    var day = dateSep[2];
 
+    document.getElementById('datetime').innerHTML = time +"<br>"+ day +"<br>"+ month;
 
+  }
 
 
 });
