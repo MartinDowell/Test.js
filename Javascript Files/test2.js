c93506ab5e36743bb66f2d0d6569a2d5c66a63f1
Martin <martin_te_dowell@dsl.pipex.com>
2018-02-17 19:28:20 +0000

    Latest

--- Javascript Files/test2.js
+++ Javascript Files/test2.js
@@ -1,22 +1,12 @@
-var fruit = [];
-for (i = 0; i < 6; i++)  {
-  fruit[i] = {};
-}
-fruit[0].name = 'lemon';
-fruit[0].colour = 'yellow';
-fruit[1].name = 'plum';
-fruit[1].colour = 'purple';
-fruit[2].name = 'apple';
-fruit[2].colour = 'green';
-fruit[3].name = 'blueberry';
-fruit[3].colour = 'blue';
-fruit[4].name = 'strawberry';
-fruit[4].colour = 'red';
-fruit[5].name = 'banana';
-fruit[5].colour = 'yellow';
+var day = 'day';
 
-function names(f)  {
-  return f.name;
-}
+function splat(day)  {
+  var x = this[day + i];
+  for (i=1; i<=5; i++)  {
 
-console.log(fruit.map(names));
+    console.log(x);
+    return x;
+  };
+};
+
+console.log(splat());
--- Weather-App/js/main.js
+++ Weather-App/js/main.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 
     var xhttp = new XMLHttpRequest();
-    var url = "http://api.apixu.com/v1/current.json?key=ee63afa5ce654c989c6120300172106&q=cv37";
-
+    var url = "http://api.apixu.com/v1/forecast.json?key=ee63afa5ce654c989c6120300172106&q=cv37&days=6";
     xhttp.onreadystatechange = function()  {
       if (this.readyState == 4 && this.status == 200)  {
         getWeather(this);
@@ -15,19 +14,35 @@ $(document).ready(function() {
   function getWeather(jsonRet)  {
     var jsonDoc = JSON.parse(jsonRet.responseText);
     document.getElementById('current').innerHTML = jsonDoc.current.temp_c +'&#8451';
-    document.getElementById('location').innerHTML = jsonDoc.location.name;
-
+    var loc = jsonDoc.location.name;
+    var reg = jsonDoc.location.region;
+    document.getElementById('location').innerHTML = loc + "<br>" + reg
+// Retrieve the local time info and seperate into date and time
     var dateTime = jsonDoc.location.localtime;
     var date = dateTime.split(' ');
     var time = date[1];
     var dateSep = date[0].split('-');
-    var year = dateSep[0];
-    var month = dateSep[1];
-    var day = dateSep[2];
+    var year = dateSep[0];  var month = dateSep[1];  var day = dateSep[2];
+// Turn month number into name
+    var months = ['Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var monthN = months[month - 1];
+    document.getElementById('datetime').innerHTML = time +"<br>"+ day +"<br>"+ monthN;
+// Get today's overview
+    var todayMaxT = jsonDoc.forecast.forecastday[0].day.maxtemp_c;
+    var todayMinT = jsonDoc.forecast.forecastday[0].day.mintemp_c;
+    var todayCond = jsonDoc.forecast.forecastday[0].day.condition.text;
+    document.getElementById('today').innerHTML = todayCond + "<br>" + todayMaxT + "<br>" + todayMinT;
 
-    document.getElementById('datetime').innerHTML = time +"<br>"+ day +"<br>"+ month;
+// Get next 5 days forecastday
+    for (i = 1; i <= 5; i++)  {
+      document.getElementById('day'+i).innerHTML = jsonDoc.forecast.forecastday[i].day.condition.text + "<br>"
+        + jsonDoc.forecast.forecastday[i].day.maxtemp_c + "<br>"
+        + jsonDoc.forecast.forecastday[i].day.mintemp_c;
+      }
 
-  }
+
+  };
+
 
 
 });
