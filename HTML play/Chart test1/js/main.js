5f2c89ba97c145285ca82952c10ba993c313f3db
Martin <martin_te_dowell@dsl.pipex.com>
2018-03-06 22:54:43 +0000

    Latest

--- HTML play/Chart test1/js/main.js
+++ HTML play/Chart test1/js/main.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
 
-  var ctx = document.getElementById("myChart");
+  var ctx = document.getElementById("myChart").getContext('2d');
   var myChart = new Chart(ctx, {
       type: 'bar',
       data: {
           labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
           datasets: [{
               label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
+              data: [12, 19, 3, 5, 14, 10],
               backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.3)',
+                'rgba(54, 162, 235, 0.3)',
+                'rgba(255, 206, 86, 0.3)',
+                'rgba(75, 192, 192, 0.3)',
+                'rgba(153, 102, 255, 0.3)',
+                'rgba(255, 159, 64, 0.3)'
             ],
             borderColor: [
                 'rgba(255,99,132,1)',
@@ -24,17 +24,41 @@ $(document).ready(function() {
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 3
         }]
     },
-    options: {
-        scales: {
+    options:{
+      scales: {
             yAxes: [{
                 ticks: {
                     beginAtZero:true
                 }
             }]
+        },
+      title:{
+        display:true,
+        text:'Nations Favourite Colours',
+        fontSize:25
+      },
+      legend:{
+        display:true,
+        position:'right',
+        labels:{
+          fontColor:'#000'
         }
+      },
+      layout:{
+        padding:{
+          left:50,
+          right:0,
+          bottom:0,
+          top:0
+        }
+      },
+      tooltips:{
+        enabled:true
+      }
+
     }
       });
 
