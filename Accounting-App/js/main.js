dc18caa4ddd07722cc2fdd277ded8f144370859c
Martin <martin_te_dowell@dsl.pipex.com>
2018-03-15 21:00:42 +0000

    Latest

--- Accounting-App/js/main.js
+++ Accounting-App/js/main.js
@@ -7,20 +7,48 @@ $('.title-data').click(function()  {
 $('.title-charts').click(function()  {
   $('.title-charts').toggleClass('titleSelect');
 });
-var balanceVal = '0';
+
+var total = [];
+var id = 100;
+
+function sumArray()  {
+  balance = 0;
+  for (i = 0; i < total.length; i++)  {
+    balance += parseInt(total[i]);
+  }
+  balanceVal = balance.toString();
+  return balanceVal;
+}
+
+function deleteRow(id){
+  alert('splat!');
+   // $('#' + id).remove();
+}
+
+function addRow(dateVal, typeVal, commentsVal, amountVal)  {
+
+  $('table').append('<tr id='+ id + '>' + '<td>' + dateVal + '</td>' +
+                            '<td>' + typeVal + '</td>' +
+                            '<td>' + commentsVal + '</td>' +
+                            '<td>' + '£' + amountVal + '</td>' +
+                            '<td>' + '£' + balanceVal + '</td>' +
+                            '<td>' + '<input type="button" value="Del" onclick="deleteRow('+id+')"/>' + '</td>' +
+                            '<td>' + id + '</td>' +
+                    '</tr>');
+                    alert(id);
+  id +=1;
+  return id;
+}
+
+
 $('button').click (function()  {
-  var newTotal = '0';
   var dateVal = $('input[name = date]').val();
   var typeVal = $('input[name = type]').val();
   var commentsVal = $('input[name = comments]').val();
   var amountVal = $('input[name = amount]').val();
-  var newTotal = (parseInt(amountVal) + parseInt(newTotal) + parseInt(balanceVal)).toString();
-  $('table').append('<tr>' + '<td>' + dateVal + '</td>' +
-                            '<td>' + typeVal + '</td>' +
-                            '<td>' + commentsVal + '</td>' +
-                            '<td>' + '£' + amountVal + '</td>' +
-                            '<td>' + '£' + newTotal + '</td>' +
-                    '</tr>');
+  total.push(amountVal);
+  sumArray();
+  addRow(dateVal, typeVal, commentsVal, amountVal);
 
 });
 
