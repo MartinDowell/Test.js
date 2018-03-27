8ecc7c6837b26c2297a494a53ed958da5faa4323
Martin <martin_te_dowell@dsl.pipex.com>
2018-03-27 20:36:06 +0100

    Latest

--- Accounting-App/js/main.js
+++ Accounting-App/js/main.js
@@ -19,37 +19,37 @@ function Transaction(date, type, comments, amount, tId)  {
   this.tId = tId;
   ledger.push(this);
   sumAmount(ledger);
-  this.balance = balanceVal;
 }
 
 function sumAmount(ledger)  {
   balance = 0;
   for (i = 0; i < ledger.length; i++)  {
     balance += parseInt(ledger[i].amount);
+    ledger[i].balance = balance.toString();
   }
-  balanceVal = balance.toString();
-  return balanceVal;
 };
 
-function deleteRow() {
-  var tId = this.tId;
+function deleteRow(tId) {
+  // var tId = this.tId;
   alert(tId);
-  ledger.splice(tId - 1, 1);
+  ledger.splice((tId - 100), 1);
     // var parent = this.parentNode.parentNode;
     // while(parent.hasChildNodes())  {
     //   parent.removeChild(parent.firstChild);
     // }
+  sumAmount(ledger);
   genTable(ledger);
 };
 
-function genTable(ledger)  {
+function genTable(ledger, tId)  {
   $('tbody').remove();
 
 
   for (i = 0; i < ledger.length; i++)  {
-    var delButton = document.createElement('button');
-    var textButton = document.createTextNode('Del');
-    delButton.appendChild(textButton);
+    // var delButton = document.createElement('button');
+    // var textButton = document.createTextNode('Del');
+    // delButton.appendChild(textButton);
+    // delButton.onclick = deleteRow(tId);
 
     $('#myTable').append('<tbody>' + '<tr>' +
         '<td>' + ledger[i].date + '</td>' +
@@ -58,14 +58,18 @@ function genTable(ledger)  {
         '<td>' + ledger[i].amount + '</td>' +
         '<td>' + ledger[i].balance + '</td>' +
         '<td>' + ledger[i].tId + '</td>' +
-        '<td>' + '</td>' +
+        '<td>' + '<button id="' + (i+100) + '" onclick="deleteRow('+ (i+100) + ')">Del</button>' + '</td>' +
     '</tr>' + '</tbody>');
-    var delCell = document.getElementById('myTable').rows[i + 1].cells[5];
-    delCell.appendChild(delButton);
-    document.getElementById('myTable').rows[i + 1].cells[5].setAttribute('id', tId);
-    delCell.onclick = deleteRow;
+    // var delCell = document.getElementById('myTable').rows[i + 1].cells[5];
+    // delCell.appendChild(delButton);
+    // document.getElementById('myTable').rows[i + 1].cells[5].setAttribute('id', tId);
+    // <button id="x" onclick = "deleteRow(x)">Del</button>
+   // '<td>' + '<button id="' + tId +'" onclick="deleteRow('+ (i+100) + ')">Del</button>' + '</td>' +
+
     }
 };
+
+// delCell.onclick = deleteRow(this.id);
 
 // $('#100').click(function()  {
 //   // var db = document.getElementById(100);
@@ -75,20 +79,16 @@ function genTable(ledger)  {
 //   genTable(ledger);
 // });
 
-$('button').click (function()  {
+$('.transact').click (function()  {
   var dateVal = $('input[name = date]').val();
   var typeVal = $('input[name = type]').val();
   var commentsVal = $('input[name = comments]').val();
   var amountVal = $('input[name = amount]').val();
   var transaction = new Transaction(dateVal, typeVal, commentsVal, amountVal, tId);
-  genTable(ledger);
+  genTable(ledger, tId);
   tId +=1;
 
 });
-
-
-
-
 
 
 
