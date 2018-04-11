afbb5580c4f684eac04a6dbd5d68d304f27cb8e7
Martin <martin_te_dowell@dsl.pipex.com>
2018-04-11 23:30:38 +0100

    Latest

--- Prison Ship/js/main.js
+++ Prison Ship/js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
 
+// animateTheMan();
 
+  $('.test').click(function()  {
+    $('.test2').toggleClass('visible');
+  })
 
+// function animateTheMan() {
+//   $('.walk').animate({left:"600px"}, 5000, function()  {$('.walk').animate({left:"600px"}, 5000)});
+// }
 
 
 
