b5f86c05ed9b094fba72954150ae04adfe01ae42
Martin <martin_te_dowell@dsl.pipex.com>
2018-07-04 22:23:43 +0100

    Latest

--- Exercises/Task3/js/main.js
+++ Exercises/Task3/js/main.js
@@ -1,8 +1,21 @@
-$(document).ready(function() {
+var box = document.getElementById('numbers');
+array = [];
 
+  for (var i = 1; i <= 100; i++)  {
 
+    if (i % 3 == 0)  {
+      if (i % 5 == 0)  {
+        num = 'FooBar';
+      }
+      else num = 'Foo';
+    }
 
+    else if (i % 5 == 0)  {
+      num = 'Bar';
+    }
+    else num = i;
 
+    array.push(num);
+  }
 
-
-});
+box.innerHTML = array.join(',  ');
