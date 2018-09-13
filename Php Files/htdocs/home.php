f3f9e8d90db05f03a3aff2c783d787441a0cd5b2
Martin <martin_te_dowell@dsl.pipex.com>
2018-09-13 16:50:22 +0100

    Latest

---
+++ Php Files/htdocs/home.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id']))  {
+  require('login_tools.php');
+  load();
+}
+
+$page_title = 'Home';
+include('includes/header.html');
+
+echo "<h1>HOME</h1>
+      <p>You are now looged in,
+      {$_SESSION['first_name']} {$_SESSION['last_name']}
+      </p>";
+
+echo '<p>
+      <a href = "forum.php">Forum</a> |
+      <a href = "shop.php">Shop</a> |
+      <a href = "goodbye.php">Logout</a>
+      </p>';
+
+?>
